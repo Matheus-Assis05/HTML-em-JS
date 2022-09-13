ac227b7dc39ce55aa39a792ba21cf3dfea34277d
Matheus <22000500@aluno.cotemig.com.br>
2022-09-13 16:24:55 -0300

    projeto FINALIZADO

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,19 @@
 * IREI RECEBER A DIV DE CLASSE POPUP, QUE POR PADRAO POSSUI DISPLAY: NONE E ATRAVEZ DO JS FAZER COM QUE AO APERTAR O BOTAO O DISPLAY SE TORNE INLINE
 * TAMBEM QUERO FAZER COM QUE O POPUP FECHE AO APERTAR O X VERMELHO
 */
+
+//event listener de clicar no botao e abrir o popup
+const botao = document.querySelector('#botaoPag');
+    
+    botao.addEventListener('click', e =>{
+        const box = document.querySelector('.popup')
+        box.style.display = "inline";
+    })
+
+//botao de fechar
+const btnX = document.querySelector('.botaofechar')
+
+    btnX.addEventListener('click', e =>{
+        const box = document.querySelector('.popup')
+        box.style.display = "none";
+    })
